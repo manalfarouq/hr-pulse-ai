a88258004b41638d000b5d6f7d49780aebe87b13
Manal Farouqi <farouqimanal@gmail.com>
2026-03-02 13:30:35 +0000

    update

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,21 @@
-# Remplace hrpulse-language par ai-language-hr-pulse
-data "azurerm_cognitive_account" "language" {
-  name                = "ai-language-hr-pulse"
-  resource_group_name = var.resource_group_name
+provider "azurerm" {
+  features {}
+}
+
+# 1. On pointe vers le serveur du formateur
+data "azurerm_mssql_server" "formateur_server" {
+  name                = "sql-server-hr-pulse-2026"
+  resource_group_name = "RG-HR-PULSE-MGMT-YENNAYA" # Ton RG de formateur
+}
+resource "azurerm_mssql_database" "db_student" {
+  name      = "db-manal"
+  server_id = data.azurerm_mssql_server.formateur_server.id
+
+  # Configuration Serverless
+  sku_name     = "GP_S_Gen5_1"
+  min_capacity = 0.5
+  max_size_gb  = 2
+
+  # L'option magique pour économiser :
+  auto_pause_delay_in_minutes = 15
 }
